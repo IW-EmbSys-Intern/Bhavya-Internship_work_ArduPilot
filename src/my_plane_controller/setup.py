742012aa9229abccd17f9fa1fd94c356bf80517a
bhavya <bhavyasingh2003@gmail.com>
2026-06-27 11:50:51 +0530

    Plane control added

---
+++ src/my_plane_controller/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_plane_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='indowings24',
+    maintainer_email='bhavyasingh2003@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'circle = my_plane_controller.circle:main',
+            'takeoff_mavros = my_plane_controller.takeoff_using_mavros:main'
+        ],
+    },
+)
